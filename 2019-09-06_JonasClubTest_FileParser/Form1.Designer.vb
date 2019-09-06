344c86034e3eaddfef7a88703517ab104e55c033
dustinyee <54960793+dustinyee@users.noreply.github.com>
2019-09-06 12:50:39 -0400

    Add files via upload

---
+++ 2019-09-06_JonasClubTest_FileParser/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.DirTextBox1 = New System.Windows.Forms.TextBox()
+        Me.ParseButton1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'DirTextBox1
+        '
+        Me.DirTextBox1.Location = New System.Drawing.Point(70, 16)
+        Me.DirTextBox1.Name = "DirTextBox1"
+        Me.DirTextBox1.Size = New System.Drawing.Size(472, 20)
+        Me.DirTextBox1.TabIndex = 0
+        '
+        'ParseButton1
+        '
+        Me.ParseButton1.Location = New System.Drawing.Point(236, 55)
+        Me.ParseButton1.Name = "ParseButton1"
+        Me.ParseButton1.Size = New System.Drawing.Size(75, 23)
+        Me.ParseButton1.TabIndex = 1
+        Me.ParseButton1.Text = "Parse"
+        Me.ParseButton1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Directory:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(553, 90)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ParseButton1)
+        Me.Controls.Add(Me.DirTextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DirTextBox1 As TextBox
+    Friend WithEvents ParseButton1 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ 2019-09-06_JonasClubTest_FileParser/Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports System.IO
+Imports System.Text.RegularExpressions
+
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+
+Public Class Form1
+
+    'Processes all MERGED files in the directory of the textbox and inserts them into a database
+    Private Sub ParseButton1_Click(sender As Object, e As EventArgs) Handles ParseButton1.Click
+        If Not String.IsNullOrEmpty(DirTextBox1.Text) Then
+            Dim directoryPath As String = DirTextBox1.Text
+            Dim successUploads As Integer = 0
+
+            If Directory.Exists(directoryPath) Then
+
+                Dim files() As String = IO.Directory.GetFiles(directoryPath)
+
+                For Each file As String In files
+                    'Check if the filename starts with Merged
+                    Dim fileName As String = file.Replace(directoryPath & "\", "")
+                    If Regex.IsMatch(fileName, "^MERGED.*\.csv") Then
+                        Dim dt As New System.Data.DataTable
+                        dt = csvToDatatable(file, ",")
+
+                        'Insert datatable into database if it contains data
+                        If dt.Rows.Count > 0 Then
+                            Dim sqlConn As New SqlConnection("test db connection")
+                            Dim tableName As String = "DataTable1"
+
+                            If insertDataTableSQL(sqlConn, tableName, dt) Then
+                                successUploads = successUploads + 1
+                            End If
+                        End If
+                    End If
+                Next
+
+                MsgBox(successUploads.ToString & " files uploaded")
+            End If
+        End If
+    End Sub
+
+    'Reads in a csv and inputs the data into a datatable
+    Public Function csvToDatatable(ByVal filename As String, ByVal separator As String) As DataTable
+        Dim dt As New System.Data.DataTable
+        Dim firstLine As Boolean = True
+        Try
+            If IO.File.Exists(filename) Then
+                Using sr As New StreamReader(filename)
+                    While Not sr.EndOfStream
+                        If firstLine Then
+                            firstLine = False
+                            Dim cols = sr.ReadLine.Split(separator)
+                            For Each col In cols
+                                dt.Columns.Add(New DataColumn(col, GetType(String)))
+                            Next
+                        Else
+                            Dim data() As String = sr.ReadLine.Split(separator)
+                            dt.Rows.Add(data.ToArray)
+                        End If
+                    End While
+                End Using
+            End If
+        Catch ex As Exception
+            Console.WriteLine(ex.ToString)
+            Return dt
+        End Try
+
+        Return dt
+    End Function
+
+    'Insert datatable into SQL
+    Public Function insertDataTableSQL(ByRef sqlConn As SqlConnection, ByVal tableName As String, ByRef dt As DataTable) As Boolean
+        Try
+            sqlConn.Open()
+            Using sqlTransaction As SqlTransaction = sqlConn.BeginTransaction()
+                Using sqlBulkCopy As New SqlBulkCopy(sqlConn, SqlBulkCopyOptions.Default, sqlTransaction)
+                    sqlBulkCopy.BulkCopyTimeout = 60
+                    sqlBulkCopy.BatchSize = 1000
+                    sqlBulkCopy.DestinationTableName = tableName
+
+                    For Each col In dt.Columns
+                        sqlBulkCopy.ColumnMappings.Add(col.ToString, col.ToString)
+                    Next
+
+                    Try
+                        sqlBulkCopy.WriteToServer(dt)
+                        sqlTransaction.Commit()
+                    Catch ex As Exception
+                        Console.WriteLine(ex.ToString)
+                        sqlTransaction.Rollback()
+                    End Try
+                End Using
+            End Using
+            sqlConn.Close()
+
+        Catch ex As Exception
+            Console.WriteLine(ex.ToString)
+            Return False
+        End Try
+
+        Return True
+    End Function
+
+End Class
